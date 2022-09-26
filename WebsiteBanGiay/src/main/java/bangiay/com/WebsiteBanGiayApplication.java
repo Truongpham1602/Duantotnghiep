8832cc2551e685284d0e46c3a71c6e03ea5b8376
PhamTruong16 <90512359+PhamTruong16@users.noreply.github.com>
2022-09-26 14:26:39 +0700

    dev_truong

---
+++ WebsiteBanGiay/src/main/java/bangiay/com/WebsiteBanGiayApplication.java
@@ -0,0 +1,13 @@
+package bangiay.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBanGiayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteBanGiayApplication.class, args);
+	}
+
+}
