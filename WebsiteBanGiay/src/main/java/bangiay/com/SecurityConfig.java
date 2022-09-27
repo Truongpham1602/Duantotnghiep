929d6b8944b9698d4fd5c8c3fa05fac331b3cad0
NongNghia07 <114387875+NongNghia07@users.noreply.github.com>
2022-09-27 16:19:08 +0700

    Create SecurityConfig.java

---
+++ WebsiteBanGiay/src/main/java/bangiay/com/SecurityConfig.java
@@ -0,0 +1,37 @@
+package bangiay.com;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("123"))
+		.authorities("Roler");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		
+		http.formLogin()
+		.loginPage("/login.html")
+		.loginProcessingUrl("/perform_login")
+		.defaultSuccessUrl("/login/success", false)
+		.failureUrl("/security/login/error");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() { 
+		return new BCryptPasswordEncoder();
+	}
+}
