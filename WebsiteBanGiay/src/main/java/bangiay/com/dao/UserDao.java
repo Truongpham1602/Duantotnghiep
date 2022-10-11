a97ce1d8ed88bd3e6b2838250ae0f314f59fbb0d
Nam Thành <94166771+namthanh02@users.noreply.github.com>
2022-10-11 16:55:46 +0700

    them class

--- WebsiteBanGiay/src/main/java/bangiay/com/dao/UserDao.java
+++ WebsiteBanGiay/src/main/java/bangiay/com/dao/UserDao.java
@@ -1,5 +1,11 @@
 package bangiay.com.dao;
 
-public class UserDao {
-	String passWord, email, phone_Number, address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import bangiay.com.entity.User;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Long>{
+
 }
