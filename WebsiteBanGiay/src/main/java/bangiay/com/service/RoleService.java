ff5b1c41d28a1df001fd93e394f5719978dca353
NguyenNTPH17778 <81691841+NguyenNTPH17778@users.noreply.github.com>
2022-10-29 14:56:43 +0700

    Update RoleService.java

--- WebsiteBanGiay/src/main/java/bangiay/com/service/RoleService.java
+++ WebsiteBanGiay/src/main/java/bangiay/com/service/RoleService.java
@@ -11,31 +11,4 @@ void delete(Long id);
 Role findById(Long id)throws Exception;
 List<Role> findAll();
 Role findByName(String name);
-@Service
-public class RoleService {
-	@Autowired
-	RoleDao roleDAO;
-	public Role create(Role role) {
-		return roleDAO.save(role);
-	}
-	public Role update(Role role,Long id) {
-		Role r = roleDAO.findById(id).orElse(null);
-		r.setRoleName(role.getRoleName());
-		return roleDAO.save(r);
-	}
-	public void delete (Long  id) {
-		roleDAO.delete(roleDAO.findById(id).get());
-	}
-	public Role findById(Long id)throws Exception {
-		return roleDAO.findById(id).orElseThrow(()->new Exception("Role is not exists" + id));
-	}
-	public List<Role> findAll(){
-		return roleDAO.findAll();
-	}
-	public Role findByName(String name) {
-		for(Role r : roleDAO.findAll())
-			if(r.getRoleName().equals(name))
-				return r;
-		return null;
-	}
 }
