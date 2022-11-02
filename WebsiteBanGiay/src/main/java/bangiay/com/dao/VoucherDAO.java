5e5038107907b56b8c42faa56bb6ca43b626fe3d
NongNghia07 <114387875+NongNghia07@users.noreply.github.com>
2022-11-02 14:49:46 +0700

    Update VoucherDAO.java

--- WebsiteBanGiay/src/main/java/bangiay/com/dao/VoucherDAO.java
+++ WebsiteBanGiay/src/main/java/bangiay/com/dao/VoucherDAO.java
@@ -1,10 +1,11 @@
 package bangiay.com.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 import bangiay.com.entity.Voucher;
 
-public interface VoucherDAO extends JpaRepository<Voucher, Integer>{
-	
+@Repository
+public interface VoucherDao extends JpaRepository<Voucher, Integer> {
+
 }
