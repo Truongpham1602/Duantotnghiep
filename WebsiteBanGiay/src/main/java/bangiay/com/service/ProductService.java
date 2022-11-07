d4555f51fa990b52848cedeeb76b0f28fbe17f76
NongNghia07 <114387875+NongNghia07@users.noreply.github.com>
2022-11-07 15:17:09 +0700

    u

--- WebsiteBanGiay/src/main/java/bangiay/com/service/ProductService.java
+++ WebsiteBanGiay/src/main/java/bangiay/com/service/ProductService.java
@@ -1,19 +1,19 @@
 package bangiay.com.service;
 
 import java.util.List;
+
 import bangiay.com.DTO.ProductDTO;
-import bangiay.com.entity.Product;
 
 public interface ProductService {
 	public List<ProductDTO> findAll();
-	
+
 	public ProductDTO create(ProductDTO productDTO);
-	
+
 	public ProductDTO update(ProductDTO productDTO);
-	
+
 	public ProductDTO finById(Integer id);
-	
+
 	public void delete(Integer id);
-	
-	List<Product> getAllProductByCategoryParent(Integer id);
+
+	List<ProductDTO> getAllProductByCategoryParent(Integer id);
 }
