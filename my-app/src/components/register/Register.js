e1019d2d69cb3ba4365c21ead0d43619671e9aae
NongNghia07 <114387875+NongNghia07@users.noreply.github.com>
2022-11-17 17:04:31 +0700

    Update Register.js

--- my-app/src/components/register/Register.js
+++ my-app/src/components/register/Register.js
@@ -8,7 +8,7 @@ import {
   MDBCardBody,
   MDBInput,
 }
-from 'mdb-react-ui-kit';
+  from 'mdb-react-ui-kit';
 import { NavLink } from "react-router-dom";
 import '../css/login.css';
 import { Button } from "reactstrap";
@@ -46,35 +46,34 @@ const Register = () => {
   return (
     <div className="login">
 
-    <MDBContainer fluid>
+      <MDBContainer fluid>
 
-      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-        <MDBCol col='12'>
+        <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+          <MDBCol col='12'>
 
-          <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
-            <MDBCardBody className='p-5 w-100 d-flex flex-column'>
+            <MDBCard className='bg-white my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '500px' }}>
+              <MDBCardBody className='p-5 w-100 d-flex flex-column'>
 
-              <h2 className="fw-bold mb-2 text-center">Sign up</h2>
+                <h2 className="fw-bold mb-2 text-center">Sign up</h2>
 
-              <MDBInput wrapperClass='mb-4 w-100' placeholder="Fullname" id='formControlLg' type='fullname' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' placeholder="Email" id='formControlLg' type='email' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' placeholder="Password" id='formControlLg' type='password' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' placeholder="Telephone" id='formControlLg' type='telephone' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' placeholder="Address" id='formControlLg' type='address' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' placeholder="Image" id='formControlLg' type='file' size="lg"/>
-              <hr className="my-2" />
-              <NavLink className="navbar-brand ps-2" to="/login" >Đã có tài khoản?</NavLink>
-              <hr className="my-2" />
-              <Button color="primary" to="/login">
-              Sign up
-             </Button>
-            </MDBCardBody>
-          </MDBCard>
+                <MDBInput wrapperClass='mb-4 w-100' placeholder="Fullname" id='formControlLg' type='fullname' size="lg" />
+                <MDBInput wrapperClass='mb-4 w-100' placeholder="Email" id='formControlLg' type='email' size="lg" />
+                <MDBInput wrapperClass='mb-4 w-100' placeholder="Password" id='formControlLg' type='password' size="lg" />
+                <MDBInput wrapperClass='mb-4 w-100' placeholder="Telephone" id='formControlLg' type='telephone' size="lg" />
+                <MDBInput wrapperClass='mb-4 w-100' placeholder="Address" id='formControlLg' type='address' size="lg" />
+                <hr className="my-2" />
+                <NavLink className="navbar-brand ps-2" to="/login" >Đã có tài khoản?</NavLink>
+                <hr className="my-2" />
+                <Button color="primary" to="/login">
+                  Sign up
+                </Button>
+              </MDBCardBody>
+            </MDBCard>
 
-        </MDBCol>
-      </MDBRow>
+          </MDBCol>
+        </MDBRow>
 
-    </MDBContainer>
+      </MDBContainer>
     </div>
   );
 }
