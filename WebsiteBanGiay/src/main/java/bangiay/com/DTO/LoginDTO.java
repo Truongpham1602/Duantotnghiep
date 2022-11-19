eb81d513ffc127e78ea2b2a369224d421a1153a1
hoanglong10012002 <73813706+long10012002@users.noreply.github.com>
2022-11-19 14:51:50 +0700

    update login + search

---
+++ WebsiteBanGiay/src/main/java/bangiay/com/DTO/LoginDTO.java
@@ -0,0 +1,8 @@
+package bangiay.com.DTO;
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String username;
+    private String pass;
+}
---
+++ WebsiteBanGiay/src/main/java/bangiay/com/DTO/ResponseTokenDTO.java
@@ -0,0 +1,13 @@
+package bangiay.com.DTO;
+import lombok.Data;
+
+@Data
+public class ResponseTokenDTO {
+    private String username;
+    private String token;
+
+    public ResponseTokenDTO(String token , String username) {
+        this.token = token;
+        this.username = username;
+    }
+}
